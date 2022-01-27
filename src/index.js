e67cfa8b6e89bb62f601da7ebf64a953773fce1f
Jack112-create <jackconroy777@gmail.com>
2022-01-27 12:58:43 +0000

    serve todo list app

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 
 const api = express()
+api.use(express.static(__dirname + '/public'));
+
+
 api.listen(3000, () => {
     console.log('Api up and running.');
 });
 
-api.get('/', (req, res) => {
-    res.send('Hello world!');
-})
+// api.get('/', (req, res) => {
+//     res.send('Hello world!');
+// })
